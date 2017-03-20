e5197d5824caf898dd46b836da1f7443a347397a
rakshitajain5 <rakshitajain5@gmail.com>
2017-03-20 16:58:16 +0530

    cassandra servers

--- resources/constants.go
+++ resources/constants.go
@@ -2,4 +2,4 @@ package resources
 
 var MESSAGE_BATCH_LIMIT int = 100
 var MESSAGE_HASH_KEY string = "abcd"
-var CASSANDRA_SERVERS string = "172.23.16.14,172.23.16.15,172.23.16.16"
+var CASSANDRA_SERVERS string = `"172.23.16.14", "172.23.16.15", "172.23.16.16"`
